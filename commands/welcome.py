2fa44131f04d36abf8f5bbb7751836d65a56ab3d
Bumo <40246197+bumothekid@users.noreply.github.com>
2022-03-30 23:22:18 +0200

    Welcome Setup with sqlite Intergration

--- commands/welcome.py
+++ commands/welcome.py
@@ -1,4 +1,5 @@
 import nextcord
+import sqlite3
 from nextcord.ext import commands
 from json import load, dump
 from requests import get
@@ -11,10 +12,16 @@ class Welcome(commands.Cog):
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
-        with open("data/welcome.json", encoding="utf-8") as f:
-            welcome_data = load(f)
+        # with open("data/welcome.json", encoding="utf-8") as f:
+        #     welcome_data = load(f)
+        
+        db = sqlite3.connect("database.db")
+        c = db.cursor()
+        c.execute(f"SELECT channel_id FROM welcome WHERE guild_id = '{member.guild_id}'")
+        channel_id = c.fetchone()
 
-        if welcome_data.get(str(member.guild.id)):
+        # if welcome_data.get(str(member.guild.id)):
+        if channel_id is not None:
             welcome = Image.open("assets/welcome/default.jpg")
             avatar = Image.open(BytesIO(get(member.avatar.url).content)).resize((275, 275))
 
@@ -35,22 +42,32 @@ class Welcome(commands.Cog):
             embed.set_footer(text=f"{member} | {member.id}")
             embed.set_image(url="attachment://done.jpg")
 
-            channel = self.client.get_channel(welcome_data[str(member.guild.id)]["channel_id"])
+            # channel = self.client.get_channel(welcome_data[str(member.guild.id)]["channel_id"])
+            channel = self.client.get_channel(channel_id[0])
             await channel.send(embed=embed, file=nextcord.File("assets/welcome/done.jpg"))
 
     @commands.command(aliases=["welcomeset"])
     @commands.has_guild_permissions(administrator=True)
     @commands.cooldown(2, 10, commands.BucketType.user)
     async def setwelcome(self, ctx, channel: nextcord.abc.GuildChannel):
-        with open("data/welcome.json", encoding="utf-8") as f:
-            welcome_data = load(f)
+        # with open("data/welcome.json", encoding="utf-8") as f:
+        #     welcome_data = load(f)
 
-        if not welcome_data.get(str(ctx.guild.id)):
-            welcome_data[str(ctx.guild.id)] = {}
-            welcome_data[str(ctx.guild.id)]["channel_id"] = channel.id
+        db = sqlite3.connect("database.db")
+        c = db.cursor()
+        c.execute(f"SELECT channel_id FROM welcome WHERE guild_id = '{ctx.guild.id}'")
+        exists = await c.fetchone()
 
-            with open("data/welcome.json", "w") as f:
-                dump(welcome_data, f, indent=4)
+        if exists is None:
+        # if not welcome_data.get(str(ctx.guild.id)):
+        #     welcome_data[str(ctx.guild.id)] = {}
+        #     welcome_data[str(ctx.guild.id)]["channel_id"] = channel.id
+
+
+            # with open("data/welcome.json", "w") as f:
+            #     dump(welcome_data, f, indent=4)
+
+            c.execute("INSERT INTO welcome(guild_id, channel_id) VALUES(?, ?)", [ctx.guild.id, channel.id])
 
             embed = nextcord.Embed(
                 description=f"Der Willkommenskanal wurde erfolgreich in {channel.mention} gesetzt!",
@@ -58,6 +75,7 @@ class Welcome(commands.Cog):
 
             await ctx.reply(embed=embed, mention_author=False)
 
+            db.commit()
         else:
             embed = nextcord.Embed(
                 description="Bitte entferne erst den aktuellen Willkommenskanal!",
@@ -69,14 +87,22 @@ class Welcome(commands.Cog):
     @commands.has_guild_permissions(administrator=True)
     @commands.cooldown(2, 10, commands.BucketType.user)
     async def removewelcome(self, ctx):
-        with open("data/welcome.json", encoding="utf-8") as f:
-            welcome_data = load(f)
+        # with open("data/welcome.json", encoding="utf-8") as f:
+        #     welcome_data = load(f)
 
-        if welcome_data.get(str(ctx.guild.id)):
-            del welcome_data[str(ctx.guild.id)]
+        db = sqlite3.connect("database.db")
+        c = db.cursor()
+        c.execute(f"SELECT channel_id FROM welcome WHERE guild_id = '{ctx.guild.id}'")
+        exists = await c.fetchone()
 
-            with open("data/welcome.json", "w") as f:
-                dump(welcome_data, f, indent=4)
+        if exists is not None:
+        # if welcome_data.get(str(ctx.guild.id)):
+        #     del welcome_data[str(ctx.guild.id)]
+
+            # with open("data/welcome.json", "w") as f:nm
+            #     dump(welcome_data, f, indent=4)
+
+            c.execute(f"DELETE FROM welcome WHERE guild_id = '{ctx.guild.id}'")
 
             embed = nextcord.Embed(
                 description=f"Der Willkommenskanal wurde erfolgreich zurückgesetzt!",
@@ -84,6 +110,7 @@ class Welcome(commands.Cog):
 
             await ctx.reply(embed=embed, mention_author=False)
 
+            db.commit()
         else:
             embed = nextcord.Embed(
                 description="Momentan ist kein Willkommenskanal gesetzt!",
