361962276b2e94137b72af84a0cb6d7eee3ce861
David Riegel <40246197+bumothekid@users.noreply.github.com>
2022-04-04 18:19:39 +0200

    Für ZS aufgeräumt

--- cursy.py
+++ cursy.py
@@ -37,12 +37,8 @@ extensions = [
     'commands.bot'
 ]
 
-# On Ready
 @bot.event
 async def on_ready():
-    """
-    When the bot is ready, print some stuff
-    """
     db = sqlite3.connect("database.db")
     c = db.cursor()
     c.execute(f"UPDATE cursy SET uptime = '{time()}'")
@@ -54,89 +50,76 @@ async def on_ready():
     print('═════◥◣◈◢◤═════')
     await statusTask()
 
-# Status
 async def statusTask():
-    # This is a status message.
     await bot.change_presence(activity=nextcord.Activity(type=nextcord.ActivityType.listening, name='ZS und Bumo'))
 
-# Load Extension
 @bot.command()
 async def load(interaction, ext):
-    """
-    It loads an extension
-    
-    :param interaction: The interaction object
-    :param ext: The extension to load
-    """
     db = sqlite3.connect("database.db")
     c = db.cursor()
     c.execute("SELECT developer FROM cursy")
     devs = c.fetchone()[0]
     devlist = re.findall(r"[0-9]+", devs)
+
     if str(interaction.author.id) not in devlist:
         raise NotOwner
+
     extension = f"commands.{ext}"
+
     try:
         bot.load_extension(extension)
+
         await interaction.reply(f'{extension} wurde geladen.')
     except Exception as error:
         await interaction.reply(f'{extension} konnte nicht geladen werden. [{error}]')
 
-# Unload Extension
 @bot.command()
 async def unload(interaction, ext):
-    """
-    It loads an extension
-    
-    :param interaction: The command that was sent
-    :param ext: The extension to unload
-    """
     db = sqlite3.connect("database.db")
     c = db.cursor()
     c.execute("SELECT developer FROM cursy")
     devs = c.fetchone()[0]
     devlist = re.findall(r"[0-9]+", devs)
+
     if str(interaction.author.id) not in devlist:
         raise NotOwner
+
     extension = f"commands.{ext}"
+
     try:
         bot.unload_extension(extension)
+
         await interaction.reply(f'{extension} wurde deaktiviert.')
     except Exception as error:
         await interaction.reply(f'{extension} konnte nicht deaktiviert werden. [{error}]')
 
-# Reload Extension
 @bot.command()
 async def reload(interaction, ext):
-    """
-    Reloads an extension
-    
-    :param interaction: The interaction object
-    :param ext: The extension to reload
-    :return: A string.
-    """
     db = sqlite3.connect("database.db")
     c = db.cursor()
     c.execute("SELECT developer FROM cursy")
     devs = c.fetchone()[0]
     devlist = re.findall(r"[0-9]+", devs)
+
     if str(interaction.author.id) not in devlist:
         raise NotOwner
+
     extension = f"commands.{ext}"
+
     try:
         if ext != "*":
+
             bot.reload_extension(name=extension)
-            await interaction.reply(f'{extension} wurde neu geladen.')
-            return
+
+            return await interaction.reply(f'{extension} wurde neu geladen.')
         for extension in extensions:
             bot.reload_extension(name=extension)
+        
         await interaction.reply(f"Es wurden {len(extensions)} Cog´s neu geladen.")
     except Exception as error:
         await interaction.reply(f'{extension} konnte nicht geladen werden. [{error}]')
 
-# Start
 if __name__ == '__main__':
-# This is a loop. It loops through the extensions list and tries to load them.
     for extension in extensions:
         try:
             bot.load_extension(extension)
