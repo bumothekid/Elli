de871a2b7ba175fc4d15f42456011705a8550654
David Riegel <40246197+bumothekid@users.noreply.github.com>
2022-04-17 02:54:26 +0200

    feat: :sparkles: whole welcome setup

--- commands/welcome.py
+++ commands/welcome.py
@@ -1,122 +1,116 @@
-import nextcord
 import sqlite3
+import nextcord
 from nextcord.ext import commands
-from json import load, dump
-from requests import get
-from PIL import Image, ImageDraw, ImageFont
-from io import BytesIO
+from nextcord.ext.commands import Cog
 
-class Welcome(commands.Cog):
-    def __init__(self, client):
-        self.client = client
+class welcome(Cog):
+    def __init__(self, bot):
+        self.bot = bot
 
-    @commands.Cog.listener()
-    async def on_member_join(self, member):
-        # with open("data/welcome.json", encoding="utf-8") as f:
-        #     welcome_data = load(f)
-        
+    @commands.group(name="welcome", aliases=["wel"], invoke_without_command=True)
+    async def _welcome(self, ctx):
+        # TODO: Add pictures commands to overview command
+        embed = nextcord.Embed(
+            description="**<:icon_member_joined:965033605707481128> Willkommensnachrichten\n\n> `-welcome channel set <#channel>`\n `-welcome channel remove <#channel>`\n `-welcome message <message>`\n\n> Variablen für die Willkommensnachricht `user_mention`, `user_name`, `user_discriminator`, `guild_name`, `guild_membercount`\n> Du kannst eine Willkommensnachricht mit mehreren Zeilen erstellen mit `\\n`",
+            color=nextcord.Color.blurple()
+        )
+
+        await ctx.reply(embed=embed)
+
+    @_welcome.group(name="channel", invoke_without_command=True)
+    @commands.has_permissions(manage_guild=True)
+    async def _channel(self, ctx):
+        embed = nextcord.Embed(
+            description="**Es fehlt ein benötigtes Argument.**",
+            color=nextcord.Color.dark_red()
+        )
+
+        await ctx.reply(embed=embed)
+
+    @_channel.command(name="set", aliases=["add", "update"])
+    @commands.has_permissions(manage_guild=True)
+    async def _set(self, ctx, channel: nextcord.TextChannel):
         db = sqlite3.connect("database.db")
         c = db.cursor()
-        c.execute(f"SELECT channel_id FROM welcome WHERE guild_id = '{member.guild.id}'")
+
+        c.execute("SELECT channel_id FROM welcome WHERE guild_id = ?", [ctx.guild.id])
         channel_id = c.fetchone()
 
-        # if welcome_data.get(str(member.guild.id)):
         if channel_id is not None:
-            welcome = Image.open("assets/welcome/default.jpg")
-            avatar = Image.open(BytesIO(get(member.avatar.url).content)).resize((275, 275))
-
-            mask = Image.new("L", avatar.size)
-            ImageDraw.Draw(mask).ellipse((0, 0, 275, 275), fill=255)
-
-            ImageDraw.Draw(welcome).text((40, 80), f"Willkommen\n{member.name}",
-                                         font=ImageFont.truetype("arial.ttf", size=100),
-                                         align="center", stroke_width=4, stroke_fill="black")
-
-            welcome.paste(avatar, (660, 60), mask)
-            welcome.save("assets/welcome/done.jpg")
+            c.execute("UPDATE welcome SET channel_id = ? WHERE guild_id = ?", (channel.id, ctx.guild.id))
+            db.commit()
 
             embed = nextcord.Embed(
-                title=":dizzy: Ein neues Mitglied erscheint!",
-                color=0x6850be)
+                description="**<:icon_member_joined:965033605707481128> Willkommenskanal aktualisiert**\n\n> **Kanal:** [`📎`Link](https://discord.com/channels/{ctx.guild.id}/{channel.id}/)\n> **Nachricht:** Willkommen auf guild_name, user_mention!",
+                color=nextcord.Color.dark_green()
+            )
 
-            embed.set_footer(text=f"{member} | {member.id}")
-            embed.set_image(url="attachment://done.jpg")
+            return await ctx.reply(embed=embed)
 
-            # channel = self.client.get_channel(welcome_data[str(member.guild.id)]["channel_id"])
-            channel = self.client.get_channel(channel_id[0])
-            await channel.send(embed=embed, file=nextcord.File("assets/welcome/done.jpg"))
+        c.execute("INSERT INTO welcome(guild_id, channel_id, message, picture) VALUES(?,?,?,?)", [ctx.guild.id, channel.id, "Willkommen auf guild_name, user_mention!", (None,)])
+        db.commit()
 
-    @commands.command(aliases=["welcomeset"])
-    @commands.has_guild_permissions(administrator=True)
-    @commands.cooldown(2, 10, commands.BucketType.user)
-    async def setwelcome(self, ctx, channel: nextcord.abc.GuildChannel):
-        # with open("data/welcome.json", encoding="utf-8") as f:
-        #     welcome_data = load(f)
+        embed = nextcord.Embed(
+            description="**<:icon_member_joined:965033605707481128> Willkommenskanal gesetzt**\n\n> **Kanal:** [`📎`Link](https://discord.com/channels/{ctx.guild.id}/{channel.id}/)\n> **Nachricht:** Willkommen auf guild_name, user_mention!",
+            color=nextcord.Color.dark_green()
+        )
 
+        await ctx.reply(embed=embed)
+
+    @_channel.command(name="remove", aliases=["delete", "reset"])
+    @commands.has_permissions(manage_guild=True)
+    async def _remove(self, ctx):
         db = sqlite3.connect("database.db")
         c = db.cursor()
-        c.execute(f"SELECT channel_id FROM welcome WHERE guild_id = '{ctx.guild.id}'")
-        exists = await c.fetchone()
 
-        if exists is None:
-        # if not welcome_data.get(str(ctx.guild.id)):
-        #     welcome_data[str(ctx.guild.id)] = {}
-        #     welcome_data[str(ctx.guild.id)]["channel_id"] = channel.id
+        c.execute("SELECT channel_id FROM welcome WHERE guild_id = ?", [ctx.guild.id])
+        channel = c.fetchone()
 
-
-            # with open("data/welcome.json", "w") as f:
-            #     dump(welcome_data, f, indent=4)
-
-            c.execute("INSERT INTO welcome(guild_id, channel_id) VALUES(?, ?)", [ctx.guild.id, channel.id])
-
+        if channel is None:
             embed = nextcord.Embed(
-                description=f"Der Willkommenskanal wurde erfolgreich in {channel.mention} gesetzt!",
-                color=nextcord.Color.dark_green())
+                description="**Es ist kein Willkommenskanal gesetzt**",
+                color=nextcord.Color.dark_red()
+            )
 
-            await ctx.reply(embed=embed, mention_author=False)
+            return ctx.reply(embed=embed)
 
-            db.commit()
-        else:
-            embed = nextcord.Embed(
-                description="Bitte entferne erst den aktuellen Willkommenskanal!",
-                color=nextcord.Color.red())
+        c.execute("UPDATE welcome SET channel_id = NULL WHERE guild_id = ?", [ctx.guild.id])
+        db.commit()
 
-            await ctx.reply(embed=embed, mention_author=False)
+        embed = nextcord.Embed(
+            description="**<:icon_member_joined:965033605707481128> Willkommenskanal zurückgesetzt**",
+            color=nextcord.Color.dark_green()
+        )
 
-    @commands.command(aliases=["welcomeremove"])
-    @commands.has_guild_permissions(administrator=True)
-    @commands.cooldown(2, 10, commands.BucketType.user)
-    async def removewelcome(self, ctx):
-        # with open("data/welcome.json", encoding="utf-8") as f:
-        #     welcome_data = load(f)
-
+        await ctx.reply(embed=embed)
+    
+    @_welcome.command(name="message", aliases=["text"])
+    @commands.has_permissions(manage_guild=True)
+    async def _message(self, ctx, *, message):
         db = sqlite3.connect("database.db")
         c = db.cursor()
-        c.execute(f"SELECT channel_id FROM welcome WHERE guild_id = '{ctx.guild.id}'")
-        exists = await c.fetchone()
 
-        if exists is not None:
-        # if welcome_data.get(str(ctx.guild.id)):
-        #     del welcome_data[str(ctx.guild.id)]
+        c.execute("SELECT * FROM welcome WHERE guild_id = ?", [ctx.guild.id])
+        welcome = c.fetchone()
 
-            # with open("data/welcome.json", "w") as f:nm
-            #     dump(welcome_data, f, indent=4)
-
-            c.execute(f"DELETE FROM welcome WHERE guild_id = '{ctx.guild.id}'")
-
+        if welcome is None or welcome[1] is None:
             embed = nextcord.Embed(
-                description=f"Der Willkommenskanal wurde erfolgreich zurückgesetzt!",
-                color=nextcord.Color.dark_green())
+                description="**Es ist kein Willkommenskanal gesetzt**",
+                color=nextcord.Color.dark_red()
+            )
 
-            await ctx.reply(embed=embed, mention_author=False)
+            return await ctx.reply(embed=embed)
 
-            db.commit()
-        else:
-            embed = nextcord.Embed(
-                description="Momentan ist kein Willkommenskanal gesetzt!",
-                color=nextcord.Color.red())
+        c.execute("UPDATE welcome SET message = ? WHERE guild_id = ?", [message, ctx.guild.id])
+        db.commit()
 
-            await ctx.reply(embed=embed, mention_author=False)
+        embed = nextcord.Embed(
+            description=f"**<:icon_member_joined:965033605707481128> Willkommensnachricht gesetzt\n\n> **Kanal:** {self.bot.get_channel(welcome[1]).mention}\n> **Nachricht:** {message}",
+            color=nextcord.Color.dark_green()
+        )
 
-def setup(client):
-    client.add_cog(Welcome(client))
+        await ctx.reply(embed=embed)
+    
+
+def setup(bot):
+    bot.add_cog(welcome(bot))
