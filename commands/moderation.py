9a1393deb7202b4972e6ba95cfe0dd9083d8211e
bumothekid <40246197+bumothekid@users.noreply.github.com>
2022-04-22 21:01:49 +0200

    feat: :sparkles: Added ban command

--- commands/moderation.py
+++ commands/moderation.py
@@ -1,3 +1,4 @@
+import asyncio
 import nextcord
 from nextcord.ext import commands
 from nextcord.ext.commands import Cog, BucketType
@@ -12,15 +13,15 @@ class moderation(Cog):
     async def _moderation(self, ctx):
         await infoEmbed(self.bot, ctx, "**<:icon_moderation:967038345395961896> Moderations Befehle**\n\n> `-clear <anzahl>`\n> `> kick <@user> <grund>`\n> `-ban <@user> <grund>`\n> `-addrole <@user> <@rolle>`\n> `-removerole <@user> <@rolle>`")
 
-    @commands.command(name="clear", aliases=["clr"])
+    @commands.command(name="clear", aliases=["clr", "clean"])
     @commands.has_permissions(manage_messages=True)
     @commands.cooldown(2, 20, BucketType.user)
     async def _clear(self, ctx, amount: int):
         if amount > 200:
             return await errorEmbed(self.bot, ctx, "Du kannst maximal 200 Nachrichten löschen.")
 
-        await ctx.channel.purge(limit=amount + 1, check=not messagePinned)
-        await successEmbed(self.bot, ctx, f"**<:icon_moderation:967038345395961896> {amount} Nachrichten gelöscht**", delete_after=10)
+        await ctx.channel.purge(limit=amount + 1, check=messagePinned)
+        await successEmbed(self.bot, ctx.channel, f"**<:icon_moderation:967038345395961896> {amount} Nachrichten gelöscht.**", delete_after=10)
     
     @commands.command(name="kick", aliases=["k"])
     @commands.has_permissions(kick_members=True)
@@ -40,7 +41,27 @@ class moderation(Cog):
             return await errorEmbed(self.bot, ctx, "Du kannst diesen User nicht kicken.")
 
         await member.kick(reason=reason)
-        await successEmbed(self.bot, ctx, f"**<:icon_moderation:967038345395961896> {member} wurde gekickt**", delete_after=10)
+        await successEmbed(self.bot, ctx, f"**<:icon_moderation:967038345395961896> {member} wurde gekickt.**")
+
+    @commands.command(name="ban", aliases=["b"])
+    @commands.has_permissions(ban_members=True)
+    @commands.bot_has_guild_permissions(ban_members=True)
+    @commands.cooldown(2, 20, BucketType.user)
+    async def _ban(self, ctx, member: nextcord.Member, *, reason: str = None):
+        if reason is None:
+            reason = "Kein Grund angegeben."
+
+        if member == ctx.author:
+            return await errorEmbed(self.bot, ctx, "Du kannst dich nicht selbst bannen.")
+        
+        if member == self.bot.user:
+            return await errorEmbed(self.bot, ctx, "Ich kann mich nicht selbst bannen.")
+        
+        if member.top_role.position >= ctx.author.top_role.position:
+            return await errorEmbed(self.bot, ctx, "Du kannst diesen User nicht bannen.")
+
+        await member.ban(reason=reason)
+        await successEmbed(self.bot, ctx, f"**<:icon_moderation:967038345395961896> {member} wurde gebannt.**")
 
 def setup(bot):
     bot.add_cog(moderation(bot))
