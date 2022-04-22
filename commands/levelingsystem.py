ee829286b1684551910f44e4d3398246e0791dc6
bumothekid <40246197+bumothekid@users.noreply.github.com>
2022-04-22 23:25:56 +0200

    feat: :rocket: Initializing LevelingSystem

---
+++ commands/levelingsystem.py
@@ -0,0 +1,12 @@
+import nextcord
+from nextcord.ext import commands
+from nextcord.ext.commands import Cog
+
+class levelingSystem(Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    
+    
+    
+def setup(bot):
+    bot.add_cog(levelingSystem(bot))
