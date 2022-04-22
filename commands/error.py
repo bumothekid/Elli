0b73705df6ee0434b361ea939cc1237ab1a49df1
bumothekid <40246197+bumothekid@users.noreply.github.com>
2022-04-22 18:58:42 +0200

    fix: :bug: Error Handling Missing Permissions for Bot and User fixed

--- commands/error.py
+++ commands/error.py
@@ -1,6 +1,7 @@
 import nextcord
 from nextcord.ext import commands
 from nextcord.ext.commands import Cog
+from nextcord import Forbidden
 from .utils.embeds import errorEmbed, errorLogging
 
 class errorhandler(Cog):
@@ -10,18 +11,21 @@ class errorhandler(Cog):
     @Cog.listener()
     async def on_command_error(self, ctx, error):
         if isinstance(error, commands.CommandNotFound):
-            await errorEmbed(self, ctx, "Dieser Command existiert nicht oder ist gerade deaktiviert.")
-
-        elif isinstance(error, commands.BotMissingPermissions):
-            await errorEmbed(self, ctx, "Dieser Bot hat nicht genug Berechtigungen.")
+            await errorEmbed(self, ctx, "Dieser Befehl existiert nicht oder ist gerade deaktiviert.")
 
         elif isinstance(error, commands.MissingRequiredArgument):
             await errorEmbed(self, ctx, "Es fehlt ein benötigtes Argument.")
-        
+
+        elif isinstance(error, commands.BotMissingPermissions):
+            missing = [perm.replace("_", " ").title() for perm in error.missing_permissions]
+            premessage = "Mir fehlen folgende Berechtigungen" if len(missing) > 1 else "Mir fehlt die folgende Berechtigung"
+            await errorEmbed(self.bot, ctx, f"{premessage} um diesen Befehl auszuführen:**\n> `{', '.join(missing)}`** ")
+
         elif isinstance(error, commands.MissingPermissions):
-            missing = [perm.replace('_', ' ').replace('guild', 'server').title() for perm in error.missing_perms]
-            await errorEmbed(f"Dir fehlen folgende Berechtigung(en) um diesen Command zu nutzen {missing}.")
-            
+            missing = [perm.replace("_", " ").replace('guild', 'server').title() for perm in error.missing_permissions]
+            premessage = "Dir fehlen folgende Berechtigungen" if len(missing) > 1 else "Dir fehlt die folgende Berechnigung"
+            await errorEmbed(self.bot, ctx, f"{premessage} um diesen Befehl zu nutzen:**\n> `{', '.join(missing)}`** ")
+
         elif isinstance(error, commands.NotOwner):
             await errorEmbed(self, ctx, "Nur die Developer können diesen Befehl ausführen.")
 
@@ -41,8 +45,11 @@ class errorhandler(Cog):
             await errorEmbed(self, ctx, "Ich konnte diese Nachricht nicht finden.")
 
         elif isinstance(error, commands.CommandOnCooldown):
-            await errorEmbed(self, ctx, f"Der Command ist noch auf Cooldown, versuche es in `{error.retry_after:,.2f}` Sekunden erneut.")
-            
+            await errorEmbed(self, ctx, f"Der Befehl ist noch auf Cooldown, versuche es in `{error.retry_after:,.2f}` Sekunden erneut.")
+
+        elif isinstance(error, commands.NoPrivateMessage):
+            await errorEmbed(self, ctx, "Dieser Befehl ist nicht für Private Nachrichten verfügbar.")
+
         else:
             # await interaction.reply("**Es ⚠️ ein kritischer Fehler aufgetreten\naber keine sorge daran bist nicht du schuld.**")
             await errorEmbed(self, ctx, "<:icon_error_red:962068826311254177> Es ist ein kritischer Fehler aufgetreten.")
