c538977bc5b1ca3758f9a021a58c3c1406cab271
bumothekid <40246197+bumothekid@users.noreply.github.com>
2022-04-26 19:14:23 +0200

    feat: :sparkles: avatar command added

--- commands/useful.py
+++ commands/useful.py
@@ -41,6 +41,19 @@ class useful(Cog):
             f"**{ctx.guild.name}'s Serverinfo**\n\n> **Server Name:** `{ctx.guild.name}`\n> **Server ID: `{ctx.guild.id}`**\n> **Server Owner: `{ctx.guild.owner}`**\n\n> **Verifikations Stufe: `{capString(str(ctx.guild.verification_level))}`**\n> **Boost Stufe: `{ctx.guild.premium_tier}`**\n> **Boost Anzahl: `{ctx.guild.premium_subscription_count}`**\n\n> **Mitglieder: `{len(ctx.guild.members)}`**\n> **Server Icon:** [`📎` Link]({ctx.guild.icon.url})\n> **Server Erstellt: <t:{int(ctx.guild.created_at.timestamp())}:R>**",
             thumbnail=ctx.guild.icon.url
         )
+    
+    @commands.command(name="avatar", aliases=["pfp", "profile", "av"])
+    @commands.cooldown(2, 20, commands.BucketType.user)
+    async def _avatar(self, ctx, member: nextcord.Member = None):
+        if member is None:
+            member = ctx.author
+
+        await infoEmbed(
+            self,
+            ctx,
+            f"**{member.name}'s Avatar**\n\n> **Avatar: [`📎` Link]({member.display_avatar.url})**",
+            thumbnail=member.display_avatar.url
+        )
 
 def setup(bot):
     bot.add_cog(useful(bot))
