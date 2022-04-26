6f992a0c7da80ecbef9aaa71056d06dbb0a59496
bumothekid <40246197+bumothekid@users.noreply.github.com>
2022-04-26 18:44:25 +0200

    feat: :sparkles: userinfo command added

--- commands/useful.py
+++ commands/useful.py
@@ -2,6 +2,8 @@ import nextcord
 
 from nextcord.ext import commands
 from nextcord.ext.commands import Cog
+
+from .utils.other import capString
 from .utils.embeds import successEmbed, errorEmbed, infoEmbed
 
 class useful(Cog):
@@ -15,6 +17,19 @@ class useful(Cog):
             self,
             ctx,
             f"**{self.bot.user.name}'s Latenz**\n\n> **Latenz: `{round(self.bot.latency * 1000)}ms`**"
+        )
+
+    @commands.command(name="userinfo", aliases=["user", "ui"])
+    @commands.cooldown(2, 20, commands.BucketType.user)
+    async def _userinfo(self, ctx, member: nextcord.Member = None):
+        if member is None:
+            member = ctx.author
+
+        await infoEmbed(
+            self,
+            ctx,
+            f"**{member.name}'s Userinfo**\n\n> **User Name:** `{member}`\n> **User ID: `{member.id}`**\n> **Status: `{capString(str(member.status))}`**\n\n> **Avatar: [`📎` Link]({member.display_avatar.url})**\n> **Gejoint: **<t:{int(member.joined_at.timestamp())}:R>\n> **Erstellt: <t:{int(member.created_at.timestamp())}:R>**",
+            thumbnail=member.display_avatar.url
         )
 
 def setup(bot):
