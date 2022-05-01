e0dc3573122fa6a92242b34311415ebbc3cfe31e
bumothekid <40246197+bumothekid@users.noreply.github.com>
2022-05-01 10:30:28 +0200

    feat: :sparkles: show command added and permission check

--- commands/automod.py
+++ commands/automod.py
@@ -9,11 +9,13 @@ class automod(Cog):
 
     @commands.group(name="badword", aliases=["bad-word", "bad_word", "wordblacklist", "word_blacklist", "word-blacklist"], invoke_without_command=True)
     @commands.cooldown(2, 20, commands.BucketType.user)
+    @commands.has_guild_permissions(manage_guild=True)
     async def _badword(self, ctx):
         await infoEmbed(self.bot, ctx, "**<:icon_badword:970238990743658518> Bad Words**\n\n> `-badword add <word>`\n> `-badword remove <word>`\n> `-badword show`")
 
     @_badword.command(name="add", aliases="a")
     @commands.cooldown(5, 30, commands.BucketType.user)
+    @commands.has_guild_permissions(manage_guild=True)
     async def _add(self, ctx, word):
         exists = readOne("word", "badwords", "guild_id word", [ctx.guild.id, word.lower()])
 
@@ -25,6 +27,7 @@ class automod(Cog):
 
     @_badword.command(name="remove", aliases=["del", "delete"])
     @commands.cooldown(5, 30, commands.BucketType.user)
+    @commands.has_guild_permissions(manage_guild=True)
     async def _remove(self, ctx, word):
         exists = readOne("word", "badwords", "guild_id word", [ctx.guild.id, word.lower()])
 
@@ -33,6 +36,21 @@ class automod(Cog):
         
         delete("badwords", "guild_id word", [ctx.guild.id, word.lower()])
         await successEmbed(self.bot, ctx, f"**{word} wurde erfolgreich entfernt.**")
+
+    @_badword.command(name="list", aliases=["show"])
+    @commands.cooldown(2, 20, commands.BucketType.user)
+    @commands.has_guild_permissions(manage_guild=True)
+    async def _list(self, ctx):
+        words = readAll("word", "badwords", "guild_id", [ctx.guild.id])
+
+        if not words:
+            return await errorEmbed(self.bot, ctx, "Es gibt noch keine Badwords.")
+
+        string = "".join(f"{word}\n" for word in words)
+
+        await infoEmbed(self.bot, ctx, f"**<:icon_badword:970238990743658518> Bad Words**\n\n{string}")
+
+        
 
     @Cog.listener()
     async def on_message(self, message):
