b9ebd60dfb4005f698304a7a63894d50c89623c6
bumothekid <40246197+bumothekid@users.noreply.github.com>
2022-05-01 10:10:51 +0200

    feat: :sparkles: badword remove command added

--- commands/automod.py
+++ commands/automod.py
@@ -1,7 +1,7 @@
 from nextcord.ext import commands
 from nextcord.ext.commands import Cog
 from .utils.embeds import infoEmbed, errorEmbed, successEmbed
-from .utils.database import insert, readOne
+from .utils.database import delete, insert, readOne
 
 class automod(Cog):
     def __init__(self, bot):
@@ -18,10 +18,21 @@ class automod(Cog):
         exists = readOne("word", "badwords", "guild_id word", [ctx.guild.id, word])
 
         if exists is not None:
-            return await errorEmbed(self.bot, ctx, "**Dieses Wort ist bereits in den Badwords vorhanden**")
+            return await errorEmbed(self.bot, ctx, "Dieses Wort ist bereits in den Badwords vorhanden.")
         
         insert("badwords", "guild_id, word", [ctx.guild.id, word])
-        await successEmbed(self.bot, ctx, f"**{word} wurde erfolgreich hinzugefügt**")
+        await successEmbed(self.bot, ctx, f"**{word} wurde erfolgreich hinzugefügt.**")
+
+    @_badword.command(name="remove", aliases=["del", "delete"])
+    @commands.cooldown(5, 30, commands.BucketType.user)
+    async def _remove(self, ctx, word):
+        exists = readOne("word", "badwords", "guild_id word", [ctx.guild.id, word])
+
+        if exists is None:
+            return await errorEmbed(self.bot, ctx, f"{word} ist nicht auf der Badword Liste.")
+        
+        delete("badwords", "guild_id word", [ctx.guild.id, word])
+        await successEmbed(self.bot, ctx, f"**{word} wurde erfolgreich entfernt.**")
 
 def setup(bot):
     bot.add_cog(automod(bot))
