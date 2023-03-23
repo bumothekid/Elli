76453486602de81df88866d7895610960f946c82
bumothekid <40246197+bumothekid@users.noreply.github.com>
2023-03-23 16:56:33 +0100

    Redesign welcome/leave card design

--- commands/utils/image.py
+++ commands/utils/image.py
@@ -1,55 +1,38 @@
-import nextcord
-from PIL import Image, ImageDraw, ImageFont, ImageFilter
+from PIL import Image, ImageDraw, ImageFont
 from io import BytesIO
 
 async def memberCardImageProcessing(ctx, image: Image, text: str) -> Image:
     draw = ImageDraw.Draw(image)
-    primaryFont = ImageFont.truetype("assets/fonts/Centrale Sans/Centrale Sans Regular.otf", 64)
-    secondaryFont = ImageFont.truetype("assets/fonts/Centrale Sans/Centrale Sans Regular.otf", 46)
+    primaryFont = ImageFont.truetype("assets/fonts/Centrale Sans/Centrale Sans Medium.otf", 46)
+    secondaryFont = ImageFont.truetype("assets/fonts/Centrale Sans/Centrale Sans Regular.otf", 40)
+
+    # primaryFont = ImageFont.truetype("assets/fonts/Squad-Regular.otf", 64)
+    # secondaryFont = ImageFont.truetype("assets/fonts/Squad-Regular.otf", 46)
 
     buffer_avatar = BytesIO(await ctx.display_avatar.replace(format="png", size=1024, static_format="png").read())
     avatar = Image.open(buffer_avatar).convert("RGBA")
     avatar.thumbnail((225, 225), Image.ANTIALIAS)
     avatar = add_corners(avatar, 12)
 
-    _, bg_h = image.size
+    bg_w, bg_h = image.size
     offset = (20, (bg_h - 225) // 2 + 1)
     image.paste(avatar, offset, avatar)
 
-    if "W" in text:
-        draw.text((360, 80), text, (255, 255, 255), font=primaryFont)
-    else:
-        draw.text((440, 80), text, (255, 255, 255), font=primaryFont)
+    txt_w, _ = draw.textsize(text, font=primaryFont)
 
-    draw.text((440, 160), ctx.name, (255, 255, 255), font=secondaryFont)
+    calc_w = bg_w - (bg_w / 2) + ((bg_w / 2) / 2) - (txt_w / 2)
+    calc_h = bg_h / 2 - 30
+
+
+    draw.text((calc_w, calc_h), text, (255, 255, 255), font=primaryFont)
+
+    txt_w, _ = draw.textsize(ctx.name, font=secondaryFont)
+
+    calc_w = bg_w - (bg_w / 2) + ((bg_w / 2) / 2) - (txt_w / 2)
+    calc_h = bg_h / 2 + 30
+
+    draw.text((calc_w, calc_h), ctx.name, (255, 255, 255), font=secondaryFont)
     return image
-
-# def dropShadow(image: Image, offset=(0, 0), shadow=(0, 0, 0, 1), blur=5):
-#     """Add a drop shadow to the image
-    
-#     Parameters
-#     ----------
-#     image : PIL.Image.Image
-#         Image to add the drop shadow to
-#     offset : tuple
-#         Offset of the shadow
-#     shadow : tuple
-#         Shadow color
-#     blur : int
-#         Blur radius
-    
-#     Returns
-#     -------
-#     PIL.Image.Image
-#         Image with drop shadow
-#     """
-#     if isinstance(shadow, (list, tuple)):
-#         shadow = Image.new("RGBA", image.size, shadow)
-#     else:
-#         shadow = shadow.convert("RGBA")
-#     shadow.alpha_composite(image, offset)
-#     shadow = shadow.filter(ImageFilter.GaussianBlur(blur))
-#     return shadow
 
 def add_corners(image, radius):
     circle = Image.new('L', (radius * 2, radius * 2), 0)
